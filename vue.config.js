75f8064bf1925a8737c8471f22cf6c062130cabd
e0392448 <834767813@qq.com>
2020-07-27 17:15:30 +0800

    init with pics

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      // 配置别名
+      alias: {
+        'components': '@/components',
+        'content': 'components/content',
+        'common': 'components/common',
+        'assets': '@/assets',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
