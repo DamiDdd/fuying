ca6a8fc88c2d786af5d3d544026dccbb96594d0e
e0392448 <834767813@qq.com>
2020-07-30 11:33:40 +0800

    head

--- fuying/src/components/content/home/HomeHead.vue
+++ fuying/src/components/content/home/HomeHead.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="home-head"></div>
+  <div id="home-head">
+    <div class="left-text">
+      <p>感觉身上不适，</p>
+      <p>但是医院各项检查指标均正常？</p>
+      <p class="bottom">您的身体可能处于<span>亚健康状态！</span></p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomeHead",
-
 }
 </script>
 
@@ -14,5 +19,20 @@ export default {
     width: var(--screen-width);
     height: 960px;
     background: url("~assets/img/bg/index1-bg.png") no-repeat top right;
+  }
+  .left-text{
+    width: 600px;
+    height: 400px;
+    position: relative;
+    margin-left: 300px;
+    padding-top: 260px;
+    font-weight: bold;
+    font-size: var(--big-text);
+  }
+  .bottom{
+    padding-top: 100px;
+  }
+  .bottom span{
+    color: var(--theme-color);
   }
 </style>
