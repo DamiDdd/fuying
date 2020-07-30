23da80ca7db8e8e0fa974d6d426cbfb0fa01ad4f
e0392448 <834767813@qq.com>
2020-07-30 17:24:43 +0800

    home1.1

--- fuying/src/components/content/home/HomeProduct.vue
+++ fuying/src/components/content/home/HomeProduct.vue
@@ -21,15 +21,27 @@
         
       </div>
       <div id="intro-middle">
-        <div class="right">
-        
-        </div>
         <div class="left">
-        
+          <img src="~assets/img/home/transcription.png">
+        </div>
+        <div class="right smalltext-blue">
+          <p>1、蛋白质组是基因组的功能最终行动者</p>
+          <p>2、与基因表达水平并非一一对应关系，基因组无法替代蛋白质组</p>
+          <p>3、蛋白质组可规避基因组、转录组层面的诸多无效信息</p>
         </div>
       </div>
       <div id="intro-bottom">
-        
+        <div class="left">
+          <p class="smalltext-blue">
+            <span class="yellow">蛋白质组学-生理刻画</span>是通过检测客户生理功能
+            （包括常规指标、免疫力指数、器官功能、代谢功能、机体衰老功能等）
+            相关的蛋白信息，准确把握生理状态，这个过程叫做“蛋白质组学-生理刻画”
+          </p>
+          <p>根据蛋白信息为您定制更系统的生活方案，让机体保持青春，让生活更加舒适。</p>
+        </div>
+        <div class="right">
+          
+        </div>
       </div>
     </div>
   </div>
@@ -70,7 +82,6 @@ export default {
     position: relative;
     margin-left: 1000px;
     padding-top: 160px;
-    /* background: gray; */
     text-align: center;
   }
 
@@ -87,7 +98,6 @@ export default {
   #pre-intro{
     width: var(--screen-width);
     height: var(--screen-height);
-    /* background: gray; */
     text-align: center;
   }
 
@@ -98,28 +108,79 @@ export default {
     margin-right: auto;
   }
 
-  .bottom{
+  #pre-intro .bottom{
     margin-top: 350px;
   }
 
   #intro{
     width: var(--screen-width);
-    height: 1800px;
-    background: -webkit-linear-gradient(top,white,lightblue,white);
+    height: 2000px;
+    /* background: -webkit-linear-gradient(top,white,lightblue,white); */
+  }
+
+  #intro div{
+    overflow: hidden;
+  }
+
+  .left{
+    float: left;
+  }
+
+  .right{
+    float: right;
   }
 
   #intro-top{
-    height: 640px;
+    height: 840px;
     background: whitesmoke;
   }
 
   #intro-middle{
     height: 260px;
-    background: beige;
   }
 
+  #intro-middle .left{
+    height: 260px;
+    width: 1100px;
+  }
+
+  #intro-middle .left img{
+    height: 200px;
+    margin-left: 220px;
+    margin-top: 30px;
+  }
+
+  #intro-middle .right{
+    height: 260px;
+    width: 800px;
+    margin-top: 50px;
+  }
+  
   #intro-bottom{
     height: 900px;
     background: gray;
+  }
+
+  #intro-bottom .left{
+    height: 900px;
+    width: 800px;
+    background: gray;
+  }
+
+  #intro-bottom .left p{
+    width: 500px;
+    line-height: 30px;
+    margin-left: 170px;
+    margin-top: 200px;
+  }
+
+  #intro-bottom .right{
+    height: 900px;
+    width: 1100px;
+    background: lightblue;
+  }
+
+  .yellow{
+    color: #ffb658
   }
 </style>
