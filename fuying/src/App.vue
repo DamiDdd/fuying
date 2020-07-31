848ca1e9bfde4ea6c89d854f0d5997a89e92030e
e0392448 <834767813@qq.com>
2020-07-31 17:18:27 +0800

    backToTop

--- fuying/src/App.vue
+++ fuying/src/App.vue
@@ -4,6 +4,14 @@
     <router-view/>
     <side-bar></side-bar>
     <main-footer></main-footer>
+    <el-tooltip placement="top" content="返回顶部">
+      <back-to-top        
+        transitionName="fade"
+        :customStyle="myBackToTopStyle" 
+        :visibilityHeight="300" 
+        :backPosition="50"></back-to-top>
+    </el-tooltip>
+    
   </div>
 </template>
 
@@ -11,13 +19,28 @@
 import MainNavBar from 'content/mainNavBar/MainNavBar'
 import MainFooter from 'content/mainFooter/MainFooter'
 import SideBar from 'common/sideBar/SideBar'
+import BackToTop from 'common/backToTop/BackToTop'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      myBackToTopStyle: {
+        'right': '100px',
+        'bottom': '150px',
+        'width': '40px',
+        'height': '40px',
+        'border-radius': '20px',
+        'line-height': '40px', 
+        'background': '#fff'
+      }
+    }
+  },
   components: {
     MainNavBar,
     MainFooter,
     SideBar,
+    BackToTop,
   }
 }
 </script>
