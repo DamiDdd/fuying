7df8c5b6263f3f578c76d7489dc43ccf63dabbcd
e0392448 <834767813@qq.com>
2020-08-04 16:13:51 +0800

    login 3.0

--- fuying/src/views/login/childComps/Register.vue
+++ fuying/src/views/login/childComps/Register.vue
@@ -7,6 +7,13 @@
         <el-form-item label="电子邮箱" prop="email"><el-input v-model="ruleForm.email"></el-input></el-form-item>        
 		<el-form-item label="密码" prop="pass"><el-input type="password" v-model="ruleForm.pass" auto-complete="off"></el-input></el-form-item>
 		<el-form-item label="确认密码" prop="checkPass"><el-input type="password" v-model="ruleForm.checkPass" auto-complete="off"></el-input></el-form-item>
+		<el-form-item label="图片验证码" prop="verifycode">
+            <el-input v-model="ruleForm.verifycode" auto-complete="off" class="identifyinput">
+            </el-input>
+            <div  class="identifybox" @click="refreshCode">
+                <identify :identifyCode="identifyCode"></identify>
+            </div>
+        </el-form-item>
 		<el-form-item>
 			<el-button type="primary" @click="submitForm('ruleForm')">注册</el-button>
 			<el-button @click="resetForm('ruleForm')">重置</el-button>
@@ -15,13 +22,22 @@
 </template>
  
 <script>
+import Identify from 'common/identify/Identify'
+
 export default {
     name: "Register",
+    components: {
+        Identify
+    },
 	data() {
 		var validatePass = (rule, value, callback) => {
+            var regPassword =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$/;
 			if (value === '') {
 				callback(new Error('请输入密码'));
-			} else {
+            } else if(regPassword.test(value) === false){
+                callback(new Error('8-16个字符，至少1个大写字母、1个小写字母、1个数字'));
+            } 
+            else {
 				if (this.ruleForm.checkPass !== '') {
 					this.$refs.ruleForm.validateField('checkPass');
 				}
@@ -60,28 +76,64 @@ export default {
 			} else {
 				callback();
 			}
-		};
- 
+        };
+        
+        var validateVerifycode = (rule, value, callback) => {
+            let val = value.toLowerCase();
+            let idcodeStr = this.identifyCode.toLowerCase();
+			if (value === '') {
+				callback(new Error('请输入验证码'));
+			} else if (val !== idcodeStr) {
+				callback(new Error('验证码不正确'));
+			} else {
+				callback();
+			}
+        };
+
 		return {
-			activeName: 'second',
+            activeName: 'second',
+            identifyCodes: '1234567890ABCDEFGHIGKLMNoPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+            identifyCode: '',
 			ruleForm: {
                 name: '',
                 phone: '',
 				pass: '',
                 checkPass: '',
                 email: '',
+                verifycode: ''
 			},
 			rules: {
                 name: [{ required: true, message: '请输入您的名称', trigger: 'blur' }, { min: 2, max: 5, message: '长度在 2 到 5 个字符', trigger: 'blur' }],
                 phone: [{required: true, validator: validatePhone, trigger: 'blur'}],
 				pass: [{ required: true, validator: validatePass, trigger: 'blur' }],
 				checkPass: [{ required: true, validator: validatePass2, trigger: 'blur' }],
-				email: [{ required: false, validator: validateEmail, trigger: 'blur' }]
+                email: [{ required: false, validator: validateEmail, trigger: 'blur' }],
+                verifycode: [{ required: true, validator: validateVerifycode, trigger: 'blur' }]
 			}
 		};
 	},
  
+    mounted() {
+        this.identifyCode = '';
+        this.makeCode(this.identifyCodes, 4);
+    },
+
 	methods: {
+        random(min,max){
+            return Math.floor(Math.random() * (max-min) + min);
+        },
+
+        refreshCode() {
+            this.identifyCode = '';
+            this.makeCode(this.identifyCodes,4);
+        },
+
+        makeCode(o,length) {
+            for(let i = 0; i < length; i++){
+                this.identifyCode += this.identifyCodes[this.random(0,this.identifyCodes.length)];
+            }
+        },
+
 		submitForm(formName) {
 			this.$refs[formName].validate(valid => {
 				if (valid) {
@@ -98,8 +150,22 @@ export default {
 		},
  
 		resetForm(formName) {
-			this.$refs[formName].resetFields();
+            this.$refs[formName].resetFields();
 		}
 	}
 };
 </script>
+
+<style scoped> 
+    .identifyinput{
+        width: 120px;
+    }
+    .identifybox{
+        /* display: flex; */
+        float: right;
+        width: 110px;
+        height: 40px;
+        overflow: hidden;
+        background: gray;
+    }
+</style>
