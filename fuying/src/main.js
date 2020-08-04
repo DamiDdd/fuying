9d45655eb58b9c775dabd5910883429e276468d9
e0392448 <834767813@qq.com>
2020-08-04 10:55:34 +0800

    login/register 1.0

--- fuying/src/main.js
+++ fuying/src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import echarts from "echarts"
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.prototype.$echarts = echarts;
+Vue.use(ElementUI);
+// Vue.arguments(ElementUI)
 
 new Vue({
   render: h => h(App),
