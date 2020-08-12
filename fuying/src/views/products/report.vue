0be45244fefab4a38ba1fbec23d3fb3f66ff46f1
e0392448 <834767813@qq.com>
2020-08-12 12:18:23 +0800

    add cover

--- fuying/src/views/products/report.vue
+++ fuying/src/views/products/report.vue
@@ -4,11 +4,59 @@
       <el-button class="pdfBtn" @click="handleDownload">PDF下载</el-button>
     </div>
     <div id="report-div">
-      <div class="test logo-div">
+      <div class="main-div logo-div">
         <img class="logo" src="~assets/img/report/logo.png">
         <p class="bigtext-blue">蛋白质组学生理刻画<br>检测报告</p>
-        <p class="smalltext">检测编号:</p>
-        <p class="smalltext">报告日期:</p>
+        <p class="smalltext">检测编号: ******</p>
+        <p class="smalltext">报告日期: ******</p>
+      </div>
+      <div class="main-div">
+        <p class="bigtext-blue">致受检者书</p>
+        <p class="content">尊敬的 ****** 女士/先生</p>
+        <p class="content">您好！</p>
+        <p class="main-text">
+          复瑛健康（上海复瑛生物科技有限公司）是生命健康领域的新兴企业，坚持自主创新和成果转化，立足新一代蛋白质
+          组技术，基于数据驱动、健康评估的检测服务公司，旨在将生命科学、临床医学及数据挖掘等创新技术融为一体，推
+          出全新的健康评估模式服务于人类健康。</p>
+        <p class="main-text">
+          蛋白质组学生理刻画产品是利用次世代蛋白质组技术配合机器学习的大数据精准分型，1滴指尖血为样本可获得2000+
+          蛋白质检测指标，涵盖100+项生理功能，完成40+大类的生理刻画，检测数据量为常规体检的近百倍，提供更加完备的
+          个人全景生理刻画。
+        </p>
+        <p class="main-text">
+          在此我们也特别提醒您注意：受检者委托复瑛健康开展蛋白质组学生理刻画检测服务，系基于受检者生理功能相关评估结
+          果，预测无明显症状的受检者是否存在生理状态不佳的情况，该检测不是一种诊断过程，而是一种简便快速的评估方法，
+          以便提高客户的生活质量。检测结果不作为临床诊断和治疗的依据。
+        </p>
+        <p class="main-text">
+          本检测的意义在于通过较完备的个人全景式生理刻画，助您掌握健康状态，希望您对此给予充分的理解与信任，我们愿意
+          与您一同积极面对可能出现的结果。由于技术发展的局限性，个体间存在的生物学差异等原因，本报告仅对本次送检样本负责。
+        </p>
+        <p class="main-text">
+          希望您能将您的宝贵意见与建议及时反馈给我们。我们免费的服务电话是********（工作日，8:30-17:30），我们将竭诚
+          为您服务！祝您健康！
+        </p>
+        <p class="main-text">此致</p>
+        <p class="main-text" style="text-indent: 0;">敬礼!</p>
+        <p class="main-text" style="text-align: right;">上海复瑛生物科技有限公司</p>
+      </div>
+      <div class="main-div product-div">
+        <p class="bigtext-blue">产品简介</p>
+        <p class="main-text">蛋白质作为生命活动的执行者，直接反映着一个个体的生理、病理
+          情况。随着人类基因组计划的实施和推进，生命科学研究已进入了后基因组时代。蛋白质组
+          研究是后基因组时代生命科学研究的核心内容之一。蛋白质组学以细胞内全部蛋白质及其活
+          动方式为研究对象。在早期诊断、疾病预防、分型、判断预后等诸多方面都具有巨大的潜力。
+        </p>
+        <p class="main-text">现阶段，本检测平台总原始数据量达到1G；谱图数达到10万张，肽段
+          数达到5.000个以上，检测体液样本的蛋白质鉴定数可达2,000种以上。
+        </p>
+        <p class="main-text">根据上万组正常人蛋白质组数据，建立基准数据训练集。通过机器学
+          习将蛋白质组数据同临床表型进行管理关联，建立包含一百种特征的训练模型。将用户样本
+          进行蛋白质组分析后与训练集比对，可对样本进行分类或分型，并据此提供较为可靠的临床辅助结论。
+        </p>
+      </div>
+      <div class="main-div">
+        <p></p>
       </div>
       <img-paper>
         <p slot="title1">1.1.1 生理年龄指数</p>
@@ -76,11 +124,20 @@ export default {
     padding-bottom: 10px;
   }
 
-  .test{
+  .main-div{
     width: var(--paper-width);
-    height: calc(var(--paper-height) + 2px);
+    height: calc(var(--paper-height) + 1px);
     margin-bottom: 10.6px;
     overflow: hidden;
+    border: 1px solid var(--theme-color);
+  }
+
+  .main-div p{
+    margin-left: 30px;
+    margin-right: 30px;
+  }
+
+  .logo-div{
     text-align: center;
   }
 
@@ -93,9 +150,13 @@ export default {
   .logo-div .bigtext-blue{
     margin-bottom: 150px;
   }
+  
+  .main-text{
+    text-indent: 2rem;
+  }
 
-  .logo-div .smalltext{
-    margin-right: 150px;
+  .product-div{
+    line-height: 30px;
   }
 
 </style>
