126e87aea03c8036f241a608bf011eb2db00243d
e0392448 <834767813@qq.com>
2020-09-03 09:24:15 +0800

    waiting to modify

--- fuying/src/components/common/mall/ItemWindow.vue
+++ fuying/src/components/common/mall/ItemWindow.vue
@@ -1,21 +1,48 @@
 <template>
   <div id="item-window">
-    <slot></slot>
+    <div class="i-div">
+      <div class="title"><slot name="title"></slot></div>
+      <div class="img"><slot name="img"></slot></div>
+      <div class="desc"><slot name="desc"></slot></div>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "ItemWindow",
+  components:{
+  },
+  methods:{
+  }
 }
 </script>
 
 <style scoped>
-  #item-window{
-    width: 600px;
-    height: 300px;
-    padding-left: 100px;
-    padding-top: 20px;
-    background: url("~assets/img/bg/item-bg.jpg") no-repeat top right;
+  .i-div{
+    width: 400px;
+    height: 350px;
+    overflow: hidden;
+    margin-left: 100px;
+    margin-top: 20px;
+    background: url("~assets/img/bg/item-bg.jpg") repeat top right;
+    /* display: flex; */
+    float: left;
   }
+  .title{
+    /* background: #000; */
+  }
+  .img{
+    /* background: gray; */
+  }
+  .img img{
+    width: 400px;
+    height: 270px;
+  }
+  .desc{
+    background: gray;
+    margin-top: -300px;
+  }
+  
 </style>
--- fuying/src/views/products/Products.vue
+++ fuying/src/views/products/Products.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="products">
-    <item-window v-for="(item,index) in goodsList" :key="index" :goods="item">
-      {{item.title}}
-    </item-window>
+    <div class="main-div">
+      <item-window v-for="(item,index) in goodsList" :key="index">
+        <p slot="title">{{item.title}}</p>
+        <img :src="item.imgurl" slot="img">
+        <p slot="desc">{{item.desc}}</p>
+      </item-window>
+    </div>
   </div>
 </template>
 
@@ -16,31 +20,70 @@ export default {
   },
   data(){
     return{
-      goodsList:[
+    }
+  },
+  mounted(){
+  },
+  props:{
+    goodsList:{
+      type: Array,
+      default(){ 
+        return [
         {
           id: "1",
-          title:"aaa",
-          des: "hhh",
+          title:"基础套餐",
+          imgurl: "https://www.fenghbio.cn/themes/echq/images/con1.jpg",
+          desc: "基础套餐【19类刻画，44项生理指数;套餐价格仅为500元，购买享受特色服务",
         },
         {
           id: "2",
           title: "bbb",
-          des: "hhh",
-        }
-      ],
+          imgurl: "https://www.fenghbio.cn/themes/echq/images/con1.jpg",
+          desc: "hhh",
+        },
+        {      
+          id: "3",
+          title:"ccc",          
+          imgurl: "https://www.fenghbio.cn/themes/echq/images/con1.jpg",
+          desc: "hhh",
+        },
+        {
+          id: "1",
+          title:"aaa",
+          imgurl: "https://www.fenghbio.cn/themes/echq/images/con1.jpg",
+          desc: "hhh",
+        },
+        {
+          id: "1",
+          title:"aaa",
+          imgurl: "https://www.fenghbio.cn/themes/echq/images/con1.jpg",
+          desc: "hhh",
+        },
+        {
+          id: "1",
+          title:"aaa",
+          imgurl: "https://www.fenghbio.cn/themes/echq/images/con3.jpg",
+          desc: "hhh",
+        },        
+        {
+          id: "1",
+          title:"aaa",
+          imgurl: "https://www.fenghbio.cn/themes/echq/images/con2.jpg",
+          desc: "hhh",
+        },
+        ]
+      },
     }
   },
-  // props:{
-  //   goodsList:{
-  //     type: Array,
-  //     default: []
-  //   }
-  // },
 }
 </script>
 
 <style scoped>
   #products{
     min-height:500px;
+  }
+
+  .main-div{
+    padding-left:200px;
   }
 </style>
