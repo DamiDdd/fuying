8eec72483cbac8e7eff660c8d6477799abc2ea2f
e0392448 <834767813@qq.com>
2020-09-09 17:18:21 +0800

    cart 1.0

---
+++ fuying/src/components/content/cart/CartView.vue
@@ -0,0 +1,140 @@
+<template>
+  <div v-if="countExist" id="cart-view">
+    <div class="flag"></div>
+    <div class="img"><img :src="good.imgurl"></div>
+    <div class="title1" @click="jump2detail">{{good.goodTitle}}</div>
+    <div class="title2">{{good.typeTitle}}</div>
+    <div class="price-single">{{good.price}}</div>
+    <div class="num-control">
+      <div class="reduce" @click="deleteCart"><img src="~assets/img/common/delete.jpg"></div>
+      <div class="num">{{good.count}}</div>
+      <div class="add" @click="addCart"><img src="~assets/img/common/add.jpg"></div>
+    </div>
+    <div class="price-sum">{{good.priceSum}}</div>
+    <div class="delete"><span  @click="deleteReverse">删除</span>
+      <div v-show="this.deleteBtn" class="answer">
+        <button class="btn" @click="deleteConfirmed">确定</button>
+        <button class="btn" @click="deleteReverse">取消</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CartView",
+  props:{
+    good: Object,
+  },
+  data(){
+    return{
+      deleteBtn: false,
+    }
+  },
+  computed:{
+    countExist(){
+      return this.good.count > 0;
+    }
+  },
+  mounted(){
+    this.good.price = parseFloat(this.good.price).toFixed(2);
+    this.good.priceSum = parseFloat(this.good.count * this.good.price).toFixed(2);
+  },
+  methods:{
+    deleteReverse(){
+      this.deleteBtn = !this.deleteBtn;
+    },
+    jump2detail(){
+      this.$router.push({path:'/detail',query:{goodId:this.good.goodId}});
+    },
+    addCart(){
+      this.good.count++;
+      this.good.priceSum = parseFloat(this.good.count * this.good.price).toFixed(2);
+      // 向后台传输最新数据
+    },
+    deleteCart(){
+      if(this.good.count>1){
+        this.good.count--;
+        this.good.priceSum = parseFloat(this.good.count * this.good.price).toFixed(2);
+        // 向后台传输最新数据
+      }
+    },
+    deleteConfirmed(){
+      this.good.count = 0;
+      console.log(this.good.count);
+      // 向后台传输数据
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #cart-view{
+    width: 60%;
+    min-width: 800px;
+    height: 120px;
+    border: 1px solid;
+    margin-left: 400px;
+    display: flex;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+  .flag, .title1, .title2, .price-single, .num-control, .price-sum, .delete{
+    padding-top: 40px;
+  }
+  .flag{
+    width: 10%;
+  }
+  .img{
+    width: 16%;
+  }
+  .img img{
+    height: 120px;
+  }
+  .title1{    
+    width: 14%;
+    cursor: pointer;
+  }
+  .title1:hover{
+    font-weight: bold;
+  }
+  .title2{
+    width: 12%;
+  }
+  .price-single{
+    width: 12%;
+  }
+  .num-control{
+    width: 12%;
+  }
+  .price-sum{
+    width: 12%;
+  }
+  .delete{
+    width: 12%;
+    cursor: pointer
+  }
+  .delete span:hover{
+    text-decoration: line-through;
+  }
+  .reduce, .add{
+    cursor: pointer;
+  }
+  .reduce, .num, .add{
+    float: left;
+    width:33%;
+  }
+  .reduce img, .add img{
+    width: 20px;
+  }
+  .answer{
+    width:100%;
+    margin-left: 10px;
+    margin-top: 5px;
+  }
+  .btn{
+    float: left;
+    width: 60px;
+    cursor: pointer;
+  }
+</style>
--- fuying/src/views/cart/Cart.vue
+++ fuying/src/views/cart/Cart.vue
@@ -1,18 +1,88 @@
 <template>
-  <div id="cart"></div>
+  <div id="cart">
+    <p class="title">我的购物车</p>
+    <div v-for="(item,index) in goods" :key="index">
+      <cart-view :good="goods[index]"></cart-view>
+    </div>
+    <div v-if="emptyCart" class="empty" @click="jump2mall">您的购物车空空如也,去商城看看吧！</div>
+  </div>
 </template>
 
 <script>
+import lab from '../../assets/img/common/lab.png'
+import CartView from 'components/content/cart/CartView'
+
 export default {
   name: 'Cart',
+  components:{
+    CartView,
+  },
+  computed:{
+    emptyCart(){
+      let sign = true;
+      if(this.goods.length == 0){
+        return sign;
+      }
+      else{
+        this.goods.forEach(element => {
+          if(element.count > 0){
+            console.log(element.count);
+            sign = false;
+          }
+        });
+      }
+      return sign;
+    }
+  },
   data(){
     return{
-      phone: localStorage.getItem("userPhone")
+      phone: localStorage.getItem("userPhone"),
+      goods:[
+        {
+          goodId: "001",
+          goodTitle: "生理刻画基本套餐",
+          typeId: "2",
+          typeTitle: "升级版",
+          imgurl: lab,
+          price: 200,
+          count: 1,
+          priceSum: 400,
+        },
+        {
+          goodId: "001",
+          goodTitle: "生理刻画基本套餐",
+          typeId: "1",
+          typeTitle: "基础版",
+          imgurl: lab,
+          price: 100,
+          count: 1,
+          priceSum: 200,
+        },
+      ],
+    }
+  },
+  mounted(){
+    // this.goods.forEach(element => {
+      
+    // });
+    // 在这里拿到cart数据
+  },
+  methods:{
+    jump2mall(){
+      this.$router.push('/products');
     }
   }
 }
 </script>
 
 <style scoped>
-
+  #cart{
+    min-height:800px;
+  }
+  .title{
+    margin-left: 400px;
+  }
+  .empty{
+    margin-left: 400px;
+  }
 </style>
