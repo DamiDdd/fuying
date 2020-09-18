8914a1068cecfc27046546dd04f3d600ce7bfd19
e0392448 <834767813@qq.com>
2020-09-18 10:21:12 +0800

    profile

--- fuying/src/views/profile/Profile.vue
+++ fuying/src/views/profile/Profile.vue
@@ -20,15 +20,33 @@
       <div class="right">
         <div class="right-top">
           <p class="content-blue">个人信息</p>
-          <div class="info"></div>
-          <div class="image"></div>
+          <div class="info">
+            <div class="info-content">
+              <p>用户名：<input type="text" v-model="name"></p>
+              <p>手机号：{{phone}}</p>
+              <p>邮箱：<input type="text" v-model="email"></p>
+            </div>
+            <div class="btn">
+              <el-button>修改密码</el-button>
+              <el-button @click="submitMsg">提交修改</el-button>
+            </div>
+          </div>
+          <div class="image">
+            <img src="~assets/img/common/user.png">
+          </div>
         </div>
         <div class="right-middle">
           <p class="content-blue">收件人管理</p>
+          <el-table :data="addressTable" class="data_table">
+            <el-table-column prop="name" label="收件人姓名" width="100"></el-table-column>
+            <el-table-column prop="phone" label="电话" width="180"></el-table-column>
+            <el-table-column prop="address" label="地址" width="320"></el-table-column>
+            <el-table-column label="操作" width="100"></el-table-column>
+  </el-table>
         </div>
-        <div class="right-bottom">
+        <!-- <div class="right-bottom">
 
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
@@ -42,7 +60,15 @@ export default {
       phone: localStorage.getItem("userPhone"),
       name: '',
       email: '',
-      address: '',
+      addressTable:[{
+        name: "a",
+        phone: "13333333333",
+        address: "北京",
+      },{
+        name: "b",
+        phone: "13333333333",
+        address: "上海",
+      }],
     }
   },
   computed:{
@@ -59,6 +85,9 @@ export default {
     jump(link) {
       this.$router.push(link)
     },
+    submitMsg(){
+      console.log(this.name + this.email);
+    }
   }
 }
 </script>
@@ -103,7 +132,7 @@ export default {
 
   .left-bottom{
     width: 80%;
-    height: 50%;
+    height: 30%;
     margin-left: 10%;
     border: 1px solid var(--theme-color);
   }
@@ -136,20 +165,54 @@ export default {
   }
 
   .info{
-    border: 1px solid;
     width: 69%;
     height: 80%;
     float: left;
   }
 
+  .info p{
+    font-weight: bold;
+    margin-top: 5%;
+    padding-left: 5%;
+    font-size: 20px;
+  }
+
+  .info-content{
+    height: 70%;
+  }
+
+  .btn{
+    display: flex;
+    margin-left: 20%;
+  }
+
+  .btn button{
+    margin-top: 2%;
+    margin-left: 10%;
+  }
+
   .image{
-    border: 1px solid;
     width: 30%;
     height: 80%;
     float: right;
   }
 
+  .image img{
+    min-width: 200px;
+    width: 80%;
+  }
+
   .right p{
     margin-left: 5%;
+  }
+  
+  input{
+    border: none;
+    font-weight: bold;
+  }
+
+  .data_table{
+    margin-left: 5%;
+    width: 700px;
   }
 </style>
