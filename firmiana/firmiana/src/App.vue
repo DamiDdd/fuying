27a78f0a46a0966aa7d57fb9d3a14f8fe4ac8ff4
e0392448 <834767813@qq.com>
2020-10-15 17:36:56 +0800

    register new f

---
+++ firmiana/firmiana/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Register></Register>
+  </div>
+</template>
+
+<script>
+import Register from './components/Register.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Register,
+  }
+}
+</script>
+
+<style>
+@import "assets/css/normalize.css";
+</style>
