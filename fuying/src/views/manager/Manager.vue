bd20810e44cceba615da8c54f07dc810651e1958
e0392448 <834767813@qq.com>
2021-01-15 10:57:27 +0800

    manager search and sort

--- fuying/src/views/manager/Manager.vue
+++ fuying/src/views/manager/Manager.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="windows">
     <el-row class="topRow">
-      <el-input class="short-input" v-model="search_num"></el-input>
+      <el-input class="short-input" v-model="filter[0].value"></el-input>
       <el-button type="primary" class="right-btn" @click="searchNum">搜索用户</el-button>
     </el-row>
     <el-table
@@ -13,9 +13,10 @@
       tooltip-effect="dark">
       <el-table-column label="订单编号" prop="orderID" width="130" sortable="custom"></el-table-column>
       <el-table-column label="用户名" prop="username" width="130" sortable="custom"></el-table-column>
-      <el-table-column label="创建日期" prop="createtime" width="230"></el-table-column>
+      <el-table-column label="创建日期" prop="createtime" width="230" sortable="custom"></el-table-column>
       <el-table-column label="订单编号" prop="sampleID" width="130" sortable="custom"></el-table-column>
-      <el-table-column label="状态" prop="status" width="130" sortable="custom"></el-table-column>
+      <el-table-column label="状态" prop="status" width="130"></el-table-column>
+      <!-- <el-table-column label="状态" prop="status" width="130" sortable="custom"></el-table-column> -->
       <el-table-column label="操作" width="130">
         <template scope="scope">
           <el-button @click="handleEdit(scope.$index,scope.row)">
@@ -41,12 +42,13 @@
 <script>
 import GLOBAL from '@/common/const'
 import Axios from 'axios'
+// import qs from 'qs'
 
 export default {
   name : "Manager",
   data(){
     return{
-      search_num: "",
+      filter: [{value:"",field:"any"}],
       pagesize:8,
       currentPage:1,
       total: 0,
@@ -54,26 +56,12 @@ export default {
       show_data:[],
       pdfUrl: GLOBAL.urlHead2 + "get_pdf/?",
       getAllUrl: GLOBAL.urlHead2 + "get_all_report/",
+      sort:[{property:"orderID",direction:"ASC"}],
     }
   },
 
   mounted(){
-    Axios.post(this.getAllUrl).then((response) => {
-      if(response.status === 200){
-        let data = response.data;
-        console.log(data);
-        this.selected_data = data;
-        this.getShowData();
-        this.total = this.selected_data.length;
-        console.log(this.total);
-      }
-    }).catch((error)=>{
-      this.$message({
-        type: 'warning',
-        message: '后台出错',
-      });
-      console.log(error);
-    });
+    this.getAllData();
     // this.selected_data = [
     //     {orderID: "000001", username: "qjj", createtime: "2021-01-12 00:00:00", status: "new"},
     //     {orderID: "000002", username: "abb", createtime: "2021-01-12 00:00:01", status: "waiting"},
@@ -83,6 +71,39 @@ export default {
   },
 
   methods:{
+    getAllData(){
+      // let params = new URLSearchParams();
+      // params.append("id",localStorage.getItem("userPhone"));
+      // params.append("filter",filter);
+      // params.append("sort",sort);
+      // console.log(params);
+      Axios.get(this.getAllUrl,
+        {params:{
+          id: localStorage.getItem("userPhone"),
+          filter: JSON.stringify(this.filter),
+          sort: JSON.stringify(this.sort),
+        },},                   
+        {headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      })
+      .then((response) => {
+        if(response.status === 200){
+          let data = response.data;
+          console.log(data);
+          this.selected_data = data;
+          this.getShowData();
+          this.total = this.selected_data.length;
+          console.log(this.total);
+        }
+      }).catch((error)=>{
+        this.$message({
+          type: 'warning',
+          message: '后台出错',
+        });
+        console.log(error);
+      });
+    },
     getShowData(){
       console.log(this.selected_data);
       // 此处修正show_data值为展示值
@@ -100,19 +121,25 @@ export default {
       });
     },
     searchNum(){
-      console.log(this.search_num);
+      console.log(this.filter);
+      this.getAllData();
     },
     changeTableSort(column){
       console.log(column);
-      var fieldName = column.prop;
+      var filterName = column.prop;
       var sortingType = column.order;
       // 通过后台处理排序
       if(sortingType == "descending"){
-        console.log(fieldName+"降序");
+        sortingType = "DESC";
+        console.log(filterName+"降序");
       }
       else{
-        console.log(fieldName+"升序");
+        sortingType = "ASC"
+        console.log(filterName+"升序");
       }
+      this.sort[0].property = filterName;
+      this.sort[0].direction = sortingType;
+      this.getAllData();
       console.log(this.selected_data)
     },
     handleEdit(index,row){
