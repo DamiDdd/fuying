9045949169d13440320291a4974d2dcfd5bcef5b
e0392448 <834767813@qq.com>
2021-01-26 14:46:47 +0800

    upload 1.0

---
+++ fuying/src/components/common/upload/UploadImg.vue
@@ -0,0 +1,78 @@
+<template>
+<div>
+<!-- action替换为目标后台网址 -->
+<!-- data替换为商品信息、分组信息 -->
+<!-- limit为当前组别的最大上传数 -->
+<el-upload
+  :action="URL"
+  :data="goodData"
+  ref="upload"
+  :auto-upload="false"
+  :multiple="true"
+  list-type="picture-card"
+  :limit="limit"
+  :on-preview="handlePictureCardPreview"
+  :on-remove="handleRemove"
+  :on-change="handleChange"
+  :on-exceed="handleExceed"
+  >
+  <i class="el-icon-plus"></i>
+</el-upload>
+<!-- <el-button @click="sendPic" type="primary">点击上传</el-button> -->
+<el-dialog :visible.sync="dialogVisible">
+  <img width="100%" :src="dialogImageUrl" alt="">
+</el-dialog>
+</div>
+</template>
+
+<script>
+export default {
+  name: "UploadImg",
+  props:{
+    limit:{
+      type: Number,
+      default:3,
+    },
+    URL:{
+      type: String,
+      default:"#",
+    },
+    goodData:{
+      type: Object,
+    }
+  },
+  data() {
+    return {
+      dialogImageUrl: '',
+      dialogVisible: false,
+      disabled: false,
+    }
+  },
+  methods: {
+    handleRemove(file,fileList) {
+      console.log(file,fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+    handleChange(file,fileList){
+      console.log(file,fileList);
+    },
+    handleExceed(){
+      this.$message({
+        type:"warning",
+        message:"当前组最多上传"+this.limit+"个文件",
+      })
+    },
+    sendPic(){
+      console.log(this.goodData.id+this.goodData.name+"...uploading");
+      this.$refs.upload.submit();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- fuying/src/views/manager/UploadGood.vue
+++ fuying/src/views/manager/UploadGood.vue
@@ -1,14 +1,65 @@
 <template>
   <div class="main">
-    <div class="center rcorner">    
+    <div class="center rcorner">
+    
+      <div v-for="(item,i) in imgList" :key="i">
+        <upload-img ref="upload" :URL="uploadImgUrl" :limit="limit" :goodData="item"></upload-img>
+        <el-button v-show="first(i)" @click="deleteImg(i)">删除分组</el-button>
+      </div>
+      <el-button @click="addTitle">增加分组</el-button>
+      <el-button @click="sendPic" type="primary">点击上传</el-button>
     </div>
-    <div></div>
   </div>
 </template>
 
 <script>
+import UploadImg from "common/upload/UploadImg"
+
 export default {
   name: "UploadGood",
+  components:{
+    UploadImg,
+  },
+  data(){
+    return{
+      limit: 3,
+      uploadImgUrl: "#",
+      imgList: [{id:"01",name:"_slides"}],
+      title: "",
+      desc: "",
+      type: [],
+    }
+  },
+  methods:{
+    // 发送图片
+    sendPic(){
+      let refDic = this.$refs.upload;
+      refDic.forEach(element => {
+        // console.log(element);
+        element.sendPic();
+      });
+    },
+    // 增加图片组
+    addTitle(){
+      // 这里需要用push使得v-for实时更新
+      this.imgList.push({id:"01",name:"add"});
+      console.log(this.imgList);
+    },
+    // 删除图片组
+    deleteImg(i){
+      console.log(i);
+      this.imgList.splice(i,1);
+    },
+    // 判断是否为第一
+    first(i){
+      if(i !== 0){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
 }
 </script>
 
